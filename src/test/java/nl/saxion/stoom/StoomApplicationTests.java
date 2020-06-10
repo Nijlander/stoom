01c3ea49a5c7414eeec6314631c4cde0089548b1
Rens Nijland <489163@student.saxion.nl>
2020-06-10 14:16:24 +0200

    Initial commit

---
+++ src/test/java/nl/saxion/stoom/StoomApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.saxion.stoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
