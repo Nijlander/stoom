738787d89d47602f53e9818b1b381f0aed8533df
ThijsVoshaar483133 <483133@student.saxion.nl>
2020-06-18 14:07:38 +0200

    fixed thymeleaf and added Movie.java

--- src/main/java/nl/saxion/stoom/Movie.java
+++ src/main/java/nl/saxion/stoom/Movie.java
@@ -1,23 +1,19 @@
 package nl.saxion.stoom;
 
-import org.apache.catalina.filters.ExpiresFilter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/store/movie")
 public class Movie {
 
-
-
-    @GetMapping("werk")
-    public String movies(){
+    @GetMapping("")
+    public String movies(Model model){
+        model.addAttribute("movie", "test");
         return "store";
     }
 }
